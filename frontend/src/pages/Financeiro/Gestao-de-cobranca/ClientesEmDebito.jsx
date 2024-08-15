d0e17da0448a3482830f551d9d616ff0a844e19a
Denilson Nunes <denilsonnunes.19982hotmail.com>
2024-08-14 22:32:15 -0400

    .

--- frontend/src/pages/Financeiro/Gestao-de-cobranca/ClientesEmDebito.jsx
+++ frontend/src/pages/Financeiro/Gestao-de-cobranca/ClientesEmDebito.jsx
@@ -1,3 +1,4 @@
+
 import {
     Box,
     TableContainer,
@@ -9,67 +10,110 @@ import {
     Th,
     Td,
     Checkbox,
-    IconButton
+    IconButton,
+    Tag,
+    TagLabel,
+    Tooltip
 } from '@chakra-ui/react'
 
 import { SearchIcon, EmailIcon, PlusSquareIcon } from '@chakra-ui/icons';
+
+//CSS
+import styles from './ClientesEmDebito.module.css'
 
 const ClientesEmDebito = () => {
     return (
 
         <Box marginTop='60px' border='1px' borderColor='red'>
+            <Box display='flex' justifyContent='space-between'>
 
-            <h2>Clientes em Débito</h2>
+                <h2>Clientes em Débito</h2>
+                <Box>
+                    <h1>Ações em Lote</h1>
+                    <IconButton
+                        marginLeft={1}
+                        width={25}
+                        height={5}
+                        aria-label="Enviar Email"
+                        icon={<EmailIcon />}
+                    />
+                </Box>
+
+            </Box>
 
             <TableContainer >
                 <Table size='md'>
-                    <Thead>
-                        <Tr>
+                    <Thead className={styles.cabecalho_table}>
+                        <Tr >
                             <Th>
                                 <Checkbox border='0.3px' borderColor='#cbd5e1' />
                             </Th>
-                            <Th>Cliente</Th>
-                            <Th isNumeric>Total Vencido</Th>
-                            <Th isNumeric>Total a Vencer</Th>
-                            <Th isNumeric>Total DE Débito</Th>
-                            <Th isNumeric>Multas e Juros</Th>
-                            <Th isNumeric>Vencido + Correção</Th>
-                            <Th isNumeric>Total Acumulado</Th>
-                            <Th isNumeric>Qtd de Títulos</Th>
-                            <Th isNumeric>Mais Antigo</Th>
-                            <Th isNumeric>Dias Vencido</Th>
+                            <Th className={styles.cabecalho_cliente}>Cliente</Th>
+                            <Th >Total Vencido</Th>
+                            <Th >Total a Vencer</Th>
+                            <Th >Total de Débito</Th>
+                            <Th >Multas e Juros</Th>
+                            <Th >Vencido + Correção</Th>
+                            <Th >Total Acumulado</Th>
+                            <Th >Qtd de Títulos</Th>
+                            <Th >Mais Antigo</Th>
+                            <Th >Dias Vencido</Th>
+                            <Th >Status</Th>
                             <Th>Ações</Th>
                         </Tr>
                     </Thead>
 
-                    <Tbody>
-                        <Tr>
-                            <Td py={0} >
+                    <Tbody className={styles.customtable}>
+                        <Tr padding={0}>
+                            <Td padding={0} py={0} px={0}>
                                 <Checkbox border='0.3px' borderColor='#cbd5e1' />
                             </Td>
-                            <Td border='1px' borderColor='red'>Cliente teste</Td>
-                            <Td border='1px' borderColor='red' isNumeric>150.000.000,00</Td>
-                            <Td isNumeric>0</Td>
-                            <Td isNumeric>1550</Td>
-                            <Td isNumeric>50</Td>
-                            <Td isNumeric>1550</Td>
-                            <Td isNumeric>1550</Td>
-                            <Td isNumeric>3</Td>
-                            <Td>15/08/2022</Td>
-                            <Td isNumeric>225</Td>
+                            <Td >Cliente teste</Td>
+                            <Td >150.000.000,00</Td>
+                            <Td >450.000.000,00</Td>
+                            <Td >250.000.000,00</Td>
+                            <Td >350.000.000,00</Td>
+                            <Td >350.000.000,00</Td>
+                            <Td >999.000.000,00</Td>
+                            <Td >999</Td>
+                            <Td >15/08/2022</Td>
+                            <Td >225</Td>
                             <Td>
-                                <IconButton my={1}
-                                    aria-label="Visualizar"
-                                    icon={<SearchIcon />}
-                                />
-                                <IconButton my={1}
-                                    aria-label="Enviar Email"
-                                    icon={<EmailIcon />}
-                                />
-                                <IconButton my={1}
-                                    aria-label="Visualizar"
-                                    icon={<PlusSquareIcon/>}
-                                />
+                                <Tag size='sm' variant='solid' colorScheme='green'>
+                                    <TagLabel fontWeight='bold'>Cobrança Realizada</TagLabel>
+                                </Tag>
+                            </Td>
+
+                            <Td>
+                                <Tooltip label='Ver Títulos' >
+                                    <IconButton
+                                        width={25}
+                                        height={5}
+                                        aria-label="Visualizar"
+                                        icon={<SearchIcon />}
+                                    />
+                                </Tooltip>
+
+                                <Tooltip label='Enviar email'>
+                                    <IconButton
+                                        marginLeft={1}
+                                        width={25}
+                                        height={5}
+                                        aria-label="Enviar Email"
+                                        icon={<EmailIcon />}
+                                    />
+                                </Tooltip>
+
+                                <Tooltip label='Registrar Cobrança'>
+                                    <IconButton
+                                        marginLeft={1}
+                                        width={25}
+                                        height={5}
+                                        aria-label="Registrar Cobrança"
+                                        icon={<PlusSquareIcon />}
+                                    />
+                                </Tooltip>
+                    
                             </Td>
 
 
@@ -80,15 +124,46 @@ const ClientesEmDebito = () => {
                                 <Checkbox border='0.3px' borderColor='#cbd5e1' />
                             </Td>
                             <Td>ELETRICA PARANA - COUTO MAGALHAES</Td>
-                            <Td isNumeric>150.000.000,00</Td>
-                            <Td isNumeric>0</Td>
-                            <Td isNumeric>1550</Td>
-                            <Td isNumeric>50</Td>
-                            <Td isNumeric>1550</Td>
-                            <Td isNumeric>1550</Td>
-                            <Td isNumeric>3</Td>
+                            <Td>150.000.000,00</Td>
+                            <Td>450.000.000,00</Td>
+                            <Td>250.000.000,00</Td>
+                            <Td>350.000.000,00</Td>
+                            <Td>350.000.000,00</Td>
+                            <Td>999.000.000,00</Td>
+                            <Td>999</Td>
                             <Td>15/08/2022</Td>
-                            <Td isNumeric>225</Td>
+                            <Td>225</Td>
+                            <Td>
+                                <Tag size='sm' variant='solid' colorScheme='yellow'>
+                                    <TagLabel fontWeight='bold'>Realizar Cobrança</TagLabel>
+                                </Tag>
+                            </Td>
+
+                            <Td>
+                                <IconButton
+                                    width={25}
+                                    height={5}
+                                    aria-label="Visualizar"
+                                    icon={<SearchIcon />}
+                                />
+                                <IconButton
+                                    marginLeft={1}
+                                    width={25}
+                                    height={5}
+                                    aria-label="Enviar Email"
+                                    icon={<EmailIcon />}
+                                />
+                                <IconButton
+                                    marginLeft={1}
+                                    width={25}
+                                    height={5}
+                                    aria-label="Visualizar"
+                                    icon={<PlusSquareIcon />}
+                                />
+
+
+                            </Td>
+
 
                         </Tr>
                         <Tr>
@@ -96,18 +171,51 @@ const ClientesEmDebito = () => {
                                 <Checkbox border='0.3px' borderColor='#cbd5e1' />
                             </Td>
                             <Td>Cliente teste</Td>
-                            <Td isNumeric>150.000.000,00</Td>
-                            <Td isNumeric>0</Td>
-                            <Td isNumeric>1550</Td>
-                            <Td isNumeric>50</Td>
-                            <Td isNumeric>1550</Td>
-                            <Td isNumeric>1550</Td>
-                            <Td isNumeric>3</Td>
+                            <Td >150.000.000,00</Td>
+                            <Td >0</Td>
+                            <Td >1550</Td>
+                            <Td >50</Td>
+                            <Td >1550</Td>
+                            <Td >1550</Td>
+                            <Td >3</Td>
                             <Td>15/08/2022</Td>
-                            <Td isNumeric>225</Td>
+                            <Td>1500</Td>
+                            <Td>
+                                <Tag size='sm' variant='solid' colorScheme='red'>
+                                    <TagLabel fontWeight='bold'>Agendado Hoje</TagLabel>
+                                </Tag>
+                            </Td>
+
+                            <Td>
+                                <IconButton
+                                    width={25}
+                                    height={5}
+                                    aria-label="Visualizar"
+                                    icon={<SearchIcon />}
+                                />
+                                <IconButton
+                                    marginLeft={1}
+                                    width={25}
+                                    height={5}
+                                    aria-label="Enviar Email"
+                                    icon={<EmailIcon />}
+                                />
+                                <IconButton
+                                    marginLeft={1}
+                                    width={25}
+                                    height={5}
+                                    aria-label="Visualizar"
+                                    icon={<PlusSquareIcon />}
+                                />
+
+
+                            </Td>
+
                         </Tr>
                     </Tbody>
-
+                    {/*
+                    
+                    
                     <Tfoot>
                         <Tr>
                             <Th></Th>
@@ -124,6 +232,9 @@ const ClientesEmDebito = () => {
                             <Th></Th>
                         </Tr>
                     </Tfoot>
+                    
+                    
+                    */}
                 </Table>
             </TableContainer>
 
