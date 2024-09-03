c7aac6f92a2c9c3d88d70455fad423d85a8624d5
DenilsonNunes <denilsonnunesdev@gmail.com>
2024-09-03 18:50:56 -0400

    .

--- frontend/src/pages/Financeiro/CardRotinasFinanceiro/CardRotinasFinanceiro.jsx
+++ frontend/src/pages/Financeiro/CardRotinasFinanceiro/CardRotinasFinanceiro.jsx
@@ -144,7 +144,7 @@ const CardRotinasFinanceiro = () => {
                 <Card bg='#f1f5f9' height='100%'>
 
                     <CardHeader display='flex' justifyContent='center'  padding={2}>
-                        <Image src='/img/calendario-financeiro.png' maxH='4rem' maxW='4rem' alt='Clientes em debito' />
+                        <Image src='/img/receber_x_pagar.png' maxH='4rem' maxW='4rem' alt='Clientes em debito' />
                     </CardHeader>
                     <CardBody >
                         <Heading textAlign='center' size='sm'>Resumo a Receber x Pagar</Heading>
@@ -159,7 +159,7 @@ const CardRotinasFinanceiro = () => {
                 <Card bg='#f1f5f9' height='100%'>
 
                     <CardHeader display='flex' justifyContent='center'  padding={2}>
-                        <Image src='/img/calendario-financeiro.png' maxH='4rem' maxW='4rem' alt='Clientes em debito' />
+                        <Image src='/img/credito-de-cliente.png' maxH='4rem' maxW='4rem' alt='Clientes em debito' />
                     </CardHeader>
                     <CardBody >
                         <Heading textAlign='center' size='sm'>Análise Credito de Clientes</Heading>
@@ -174,7 +174,7 @@ const CardRotinasFinanceiro = () => {
                 <Card bg='#f1f5f9' height='100%'>
 
                     <CardHeader display='flex' justifyContent='center'  padding={2}>
-                        <Image src='/img/calendario-financeiro.png' maxH='4rem' maxW='4rem' alt='Clientes em debito' />
+                        <Image src='/img/movimento-caixa-banco.png' maxH='4rem' maxW='4rem' alt='Clientes em debito' />
                     </CardHeader>
                     <CardBody >
                         <Heading textAlign='center' size='sm'>Movimento de Caixa / Banco</Heading>
